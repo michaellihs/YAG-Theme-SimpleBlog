22fbb5689cb69c72eb5c2f2b60af9a4586f2c615
Michael Knoll <mimi@kaktusteam.de>
2011-03-10 00:12:33 +0100

    TASK: Initial commit

---
+++ ext_tables.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+// Include static template for SimpleBlog Theme
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', '[yag] Theme SimpleBlog');
+
+?>
